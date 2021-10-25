be25fed25512df1f73287a6d3871a0e9d06bd4ef
dedkovs <dedkovs@gmail.com>
2021-10-25 15:30:28 +0300

    initial setup

---
+++ src/redux/slices/xxxSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const initialState = {};
+
+export const xxx = createSlice({
+  name: 'xxx',
+  initialState,
+  reducers: {},
+});
+
+export const {} = xxx.actions;
+
+export default xxx.reducer;
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#89c74a',
+    },
+    secondary: {
+      main: '#ff6600',
+    },
+    lightGreen: {
+      main: '#e0ffc0',
+    },
+  },
+});
